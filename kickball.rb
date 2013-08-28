bad0f863f75f51901e3c87018a238b7f89e9f5c0
Louise Au Yeung <louise@eabx.org>
2013-08-28 16:19:21 -0400

    phase 6 complete??

--- kickball.rb
+++ kickball.rb
@@ -3,14 +3,24 @@
 
 require 'pry'
 
-def identify_winner(team_one_name, team_one_score, team_two_name, team_two_score)
-
-  if team_one_score > team_two_score
-    team_one_name
-  elsif team_one_score < team_two_score
-    team_two_name
+def identify_winner(team_one_name, team_one_score, team_two_name, team_two_score, league_destination)
+  if league_destination[team_one_name].nil?
+    league_destination[team_one_name] = []
+  end
+    if league_destination[team_two_name].nil?
+    league_destination[team_two_name] = []
   end
 
+  if team_one_score.to_i > team_two_score.to_i
+    league_destination[team_one_name] << "w"
+    league_destination[team_two_name] << "l"
+    team_one_name
+
+  elsif team_one_score.to_i < team_two_score.to_i
+    league_destination[team_two_name] << "w"
+    league_destination[team_one_name] << "l"
+    team_two_name
+  end
 end
 
 def collect_answer(team_number, type, position, destination)
@@ -19,8 +29,14 @@ def collect_answer(team_number, type, position, destination)
 end
 
 
+
+
 all_games = []
 input = ""
+league = {}
+league_results = []
+
+
 
 while input != "N"
   individual_game = []
@@ -35,14 +51,45 @@ while input != "N"
   puts "Would you like to provide another game? (Y/N)"
   input = gets.chomp.upcase
   puts
-
 end
 
-
-
-
-all_games. each_with_index do |game, index|
-  winner = identify_winner(game[0], game[1], game[2], game[3])
+all_games.each_with_index do |game, index|
+  winner = identify_winner(game[0], game[1], game[2], game[3], league)
   puts "In game #{index + 1}, #{winner} is the victor!"
 end
+
+puts
+puts
+puts
+print league
+
+
+league.each do | team_name, all_results |
+  team_league_result = ["", 0, 0, 0]
+  team_league_result[0] = team_name
+  all_results.each do | win_loss |
+    if win_loss == "w"
+      team_league_result[1] += 1
+    elsif win_loss == "l"
+      team_league_result[2] += 1
+    end
+  end
+  team_league_result[3] = team_league_result[1] - team_league_result[2]
+  league_results << team_league_result
+end
+
+puts
+puts
+puts
+print league_results
+puts
+sorted_league_results = league_results.sort_by {|item| item[3] }.reverse
+
+puts
+puts
+puts "*** League Standings ***"
+sorted_league_results.each_with_index do | league_result, index |
+  puts "#{index + 1} #{league_result[0]}: #{league_result[1]}W, #{league_result[2]}L"
+end
+
 
