5711fd73e72c6855f1d7e2b620aca176329274cf
Louise Au Yeung <louise@eabx.org>
2013-08-28 13:50:27 -0400

    phase4 functions

--- kickball.rb
+++ kickball.rb
@@ -13,28 +13,37 @@ def identify_winner(team_one_name, team_one_score, team_two_name, team_two_score
 
 end
 
+all_games = []
+
+
 input = ""
 
 while input != "N"
 
+  individual_game = []
+
   puts "What was team 1's name?"
-  team_one_name = gets.chomp
+  individual_game[0] = gets.chomp
 
   puts "What was team 1's score?"
-  team_one_score = gets.chomp
+  individual_game[1] = gets.chomp
 
   puts "What was team 2's name?"
-  team_two_name = gets.chomp
+  individual_game[2] = gets.chomp
 
   puts "What was team 2's score?"
-  team_two_score = gets.chomp
+  individual_game[3] = gets.chomp
 
-  winner = identify_winner(team_one_name, team_one_score, team_two_name, team_two_score)
+  all_games << individual_game
 
-  puts "#{winner} is the victor!"
-  puts
   puts "Would you like to provide another game? (Y/N)"
   input = gets.chomp.upcase
   puts
 
 end
+
+all_games. each_with_index do |game, index|
+  winner = identify_winner(game[0], game[1], game[2], game[3])
+  puts "In game #{index + 1}, #{winner} is the victor!"
+end
+
